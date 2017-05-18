70982ec9ae8883b53078aff39e192247ae540c7d
Elise Young <eliseyoung_4@hotmail.com>
2017-05-18 14:38:55 +1000

    password

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ console.log(token);
 
 const mlab = process.env.MLAB_PASSWORD;
 
+
 //database is called recipes
-mongoose.connect(`mongodb://elise:${mlab}@ds143201.mlab.com:43201/recipe_list_api`);
+mongoose.connect(`mongodb://elise:bambam@ds143201.mlab.com:43201/recipe_list_api`);
 const {connection: db} = mongoose;
 
 db.on('error', console.error.bind(console, 'connection error:'));
